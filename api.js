7bed32257d0cc1f4bdd6efda1af80d37ac16229d
Luz Eterna <anelastar42@gmail.com>
2025-03-17 21:59:59 +0300

    Code completed. Next step using JSDOM for render.

--- api.js
+++ api.js
@@ -1,11 +1,46 @@
+const puppeteer = require('puppeteer');
+
+
 async function GetAnnotion(id) {
+    console.log("API: annotation requested: "+id)
+
     var req = await fetch(`https://genius.com/api/annotations/${id}`)
     var json = await req.json()
-    return json.response.annotation
+    return json.response
 }
 
-async function GetGeniusBody() { //html crawler
-    return require("./preload.json")
+async function GetGeniusBody(url) { //html crawler
+    console.log("API: song page requested: "+ url)
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    
+    await page.setJavaScriptEnabled(true);  
+
+    //prevents the loading of components other than script tags.
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+        if (['image', 'stylesheet', 'font', 'script', 'xhr', 'fetch'].includes(request.resourceType())) {
+            request.abort(); 
+        } else {
+            request.continue();
+        }
+    });
+    
+    page.on("console", (msg) => console.log("Browser Logs (ignore it):", msg.text()));
+
+    await page.setExtraHTTPHeaders({
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        "Accept": "*/*"
+    });
+    await page.setBypassCSP(true);
+
+
+    await page.goto(url, { method: 'GET', waitUntil: 'domcontentloaded' });
+    var state = await page.evaluate(() => window.__PRELOADED_STATE__);
+
+    await browser.close();
+    return state;
+    //return require("./preload.json") // for test
 }
 
 exports = module.exports = {
--- main.js
+++ main.js
@@ -1,43 +1,131 @@
 const fs = require('fs')
+const puppeteer = require("puppeteer")
 const Api = require("./api")
 
-!(async()=>{
-    var preload_state = await Api.GetGeniusBody("link") //example data for test
-    var document = fs.readFileSync('style.html', 'utf-8');
+    !(async () => {
+        
+        var preload_state = await Api.GetGeniusBody("https://genius.com/Sanser-susamam-lyrics") //example data for test
+        var document = await fs.readFileSync('style.html', 'utf-8');
 
-    annotions_ids = preload_state.songPage.lyricsData.referents
+        document = document.replace("%%-root-%%", jsonParser(preload_state.songPage.lyricsData.body))
 
-    document=document.replaceAll("%%-header-%%", `${preload_state.songPage.trackingData.Title} - ${preload_state.songPage.trackingData['Primary Artist']}`)
-    document=document.replace("%%-root-%%", jsonParser(preload_state.songPage.lyricsData.body))
-    
-    //console.log(await Api.GetAnnotion(17975864))  
-    console.log(document)  
-})()
+        var annotations_document = '<hr class="divider">';
+        var annotation_ids = await preload_state.songPage.lyricsData.referents;
+
+        
+        if(annotation_ids.length > 0) {
+            console.log(`MAIN: there are ${annotation_ids.length} annotation`)
+
+        var annotation = (quote, description, id) => `<div id="annotation-${id}" class="quote-container">
+                <blockquote>
+                    <a style="all: unset; text-decoration: none" href="#annotation-lyric-${id}">${quote}</a>
+                </blockquote>
+                    <p>
+                        ${description}
+                    </p>
+                </div>
+                
+            <hr class="divider">
+            `            
+            for (const id of annotation_ids) {
+                var anno = await Api.GetAnnotion(id);  // Annotations API'den veri çekiyoruz
+                var quote = anno.referent.fragment || anno.referent.range.content;
+                var description = await jsonParser(anno.annotation.body.dom);  // JSON verisini çözüyoruz
+        
+                annotations_document += annotation(quote, description, id);
+            }
+        }
+
+        document = document.replace("%%-annotations-%%", annotations_document)
+     
+        //var argumants = (key) => preload_state.songPage.trackingData.find(val => val.key == key)
+        var songid = preload_state.songPage.song
+        var song = preload_state.entities.songs[songid]
+
+        document = document.replaceAll("%%-song-name-%%", song.titleWithFeatured)
+        document = document.replaceAll("%%-artists-%%", song.primaryArtist.name)
+        document = document.replaceAll("%%-image-%%", song.songArtImageUrl)
+
+        console.log(document)
+        console.log("MAIN: loading...")
+        await createPDF(document, `${song.primaryArtist.name} - ${song.title}`)
+        console.log("MAIN: pdf created.")
+        
+        fs.writeFile(`./${song.primaryArtist.name} - ${song.title}.html`, document, (err) => {
+            if (err) {
+                console.error('Dosya oluşturulurken hata oluştu:', err);
+            } else {
+                console.log(`MAIN: html source writed.`);
+            }
+        });
+        
+    })()
+
+    //createPDF(fs.readFileSync('source.html', 'utf-8'), "pdfname")
+
+
+    async function createPDF(htmlContent, path) {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.setContent(htmlContent);
+        await page.pdf({ path: path+".pdf", format: 'A4', printBackground: true, });
+
+        await browser.close();
+        return 
+    }
 
 function jsonParser(json) {
     var doc = ``;
-        if(Array.isArray(json)) 
-            json.forEach(e=>{doc+=jsonParser(e)}) 
-        else if(typeof json == 'string') {
+    if (Array.isArray(json))
+        json.forEach(e => {
+            doc += jsonParser(e)
+        })
+    else if (typeof json == 'string') {
         doc += json;
     } else {
-        
-        if(json.tag == "a"){ //annotations 
-            doc += `<span data-annotations-id="${json.data.id}">${jsonParser(json.children)}</span>`
-            
-        } else { 
-             doc += `<${json.tag}>`;
-        if(typeof json.children == 'string') {
-            doc += json.children;
-        } else {  
-            if(json.children != undefined) //it is json with tag 
-                doc += jsonParser(json.children)
+        if (json.tag == "inread-ad")
+            doc += '<br>'
+        else if (json.tag == "a") { //annotations 
+          
+            var attributes = ''
+            if(json.attributes) {
+                if(json?.data?.id) {
+                    delete json.attributes?.href
+                }
+
+                for (const [key, value] of Object.entries(json.attributes)) {
+                    attributes += `${key}="${value}" `;
+                  }
+                
+                  attributes = attributes.trim();
+            }
+
+            doc += `<a ${attributes} href="#annotation-${json?.data?.id}" id="annotation-lyric-${json?.data?.id}" data-annotation-id="${json?.data?.id}">${jsonParser(json.children)}</a>`
+        } else {
+           
+            var attributes = ''
+            if(json.attributes) {
+                for (const [key, value] of Object.entries(json.attributes)) {
+                    attributes += `${key}="${value}" `;
+                  }
+                
+                  attributes = attributes.trim();
+            }
+
+            doc += `<${json.tag} ${attributes}>`;
+            if (typeof json.children == 'string') {
+                doc += json.children;
+            } else {
+                if (json.children != undefined) //it is json with tag 
+                    doc += jsonParser(json.children)
+            }
+
+            if (json.tag != "br" && json.tag != "a" && json.tag != "inread-ad")
+                doc += `</${json.tag}>`;
         }
-        
-    if(json.tag != "br" && json.tag != "a")
-        doc += `</${json.tag}>`;
     }
-}
     return doc;
 }
+
 
